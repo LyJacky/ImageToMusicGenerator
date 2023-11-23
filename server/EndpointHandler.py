b02f4d4eb4759584d26483114141ff16a6d4441b
jacky <jacky.nhiayi@gmail.com>
2023-11-23 15:24:22 -0800

    saving output as a music file

--- server/EndpointHandler.py
+++ server/EndpointHandler.py
@@ -1,4 +1,8 @@
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
+import scipy
+import numpy as np
+
+
 
 processor = AutoProcessor.from_pretrained("facebook/musicgen-large")
 model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-large")
@@ -9,3 +13,4 @@ inputs = processor(
     return_tensors="pt",
 )
 audio_values = model.generate(**inputs, do_sample=True, guidance_scale=3, max_new_tokens=256)
+scipy.io.wavfile.write("musicgen_out.wav", rate=32000, data=np.array(audio_values[0]))
