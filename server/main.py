7563b9f9086b5e594ebfb1461071b404fe6233d2
jacky <jacky.nhiayi@gmail.com>
2024-01-24 11:42:34 -0800

    sending wav file to front end

--- server/main.py
+++ server/main.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 from model.CaptionsToMusic import to_musical
 from model.MusicGen import to_music
 from model.ImageCaptioner import make_captions
 from PIL import Image
+import io
+import scipy.io.wavfile as wavfile
 import torch
 app = Flask(__name__)
 CORS(app)
@@ -16,21 +18,30 @@ def get_data():
         file = request.files['image']
         img = Image.open(file.stream)
         # img.show()
-        print(to_musical("A picture of the eiffel tower at night"))
-        print(generate_captions_from_image(img))
+        # print(to_musical("A picture of the eiffel tower at night"))
+        captions = generate_captions_from_image(img)
+        print('CAPTIONS: ', captions)
+        musical_descriptions = generate_musical_description_from_caption(captions)
+        print('MUSICAL DESCRIPTION: ', musical_descriptions)
+        audio_values,sampling_rate = generate_music_from_musical_captions("test4",musical_descriptions)
         # generate_musical_description_from_caption("test4","modern pop parisian music")
-        return jsonify({"res":"i went inside the if statment", 'size': [img.width,img.height]}), 200
+        wav_buffer = io.BytesIO()
+        wavfile.write(wav_buffer, rate=sampling_rate, data=audio_values)
+        # Set the buffer position to the beginning
+        wav_buffer.seek(0)
+        return send_file(wav_buffer, mimetype="audio/wav", as_attachment=True, download_name=f"test4.wav")
+        # return jsonify({"res":"i went inside the if statment", 'size': [img.width,img.height]}), 200
     data = {"message": "Hello from Flask!"}
     return jsonify(data),200
 
-def generate_music_from_img(caption):
+def generate_music_from_musical_captions(musical_descriptions,title):
+    return to_music(musical_descriptions,title)
+
+def generate_musical_description_from_caption(caption):
     return to_musical(caption)
 
-def generate_musical_description_from_caption(title,text):
-    return to_music(title, text)
-
 def generate_captions_from_image(img):
-    return make_captions(img)
+    return make_captions(img)[0]
 
 
 if __name__ == '__main__':
--- server/model/MusicGen.py
+++ server/model/MusicGen.py
@@ -15,6 +15,8 @@ def to_music(title,text):
     audio_values = model.generate(**inputs, do_sample=True, guidance_scale=3, max_new_tokens=256)
     sampling_rate = model.config.audio_encoder.sampling_rate
     scipy.io.wavfile.write("../musicgen_out"+title+".wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
+    data = audio_values[0, 0].numpy()
+    return data,sampling_rate
 
 if __name__ == "__main__":
     to_music("test2","modern pop horror music")
